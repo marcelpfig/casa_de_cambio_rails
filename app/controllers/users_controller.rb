dc7c122385264347a9cb3599db95e5c0808b205c
Marcel Figueiredo <marcelpf_10@hotmail.com>
2018-12-19 12:00:59 -0200

    Users and transactions are being saved and shown properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,29 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to root_path, notice:'Transação apagada com sucesso!'
+    end
+
     private
     
-    def transaction_params
+    def user_params
         params.require(:user).permit(:email, :name, :cpf)
     end
 end
