53a281d3ea431234f129f953459d71b3636a23b0
Marcel Figueiredo <marcelpf_10@hotmail.com>
2018-12-21 10:07:14 -0200

    Transactions can now be filtered by currency

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
 
     def index
-        @transactions = Transaction.all
+        #@transactions = Transaction.all
+        if params[:currency]
+            @transactions = Transaction.where(currency: params[:currency])
+        else
+            @transactions = Transaction.all.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+        end
     end
 end
